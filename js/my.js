e2d7720c870ba15ae4b6fd9b30194f23627503d4
Isabel Tan <4101.isabel@gmail.com>
2021-09-26 19:39:32 -0400

    Changes Bunch

---
+++ js/my.js
@@ -0,0 +1,16 @@
+/* alert("This alert box was called with the onload event"); 
+*/
+
+
+/*$(document).ready(function() {
+  $('. carousel'). carousel({ interval: false,});
+}); 
+*/
+
+
+/*$( document ).click(function() {
+  $( "#toggle" ).toggle( "highlight" );
+});
+*/
+
+

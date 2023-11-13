37e2fb646a87d4b1039f8c63f5f264698b34d96c
isabeltan <38549280+isabeltan@users.noreply.github.com>
2023-11-13 11:49:33 +0000

    Update my.js

--- js/my.js
+++ js/my.js
@@ -1,16 +1,29 @@
-/* alert("This alert box was called with the onload event"); 
-*/
+// Function to update date and time in the specified format on one line
+const updateDateTime = () => {
+    const now = new Date();
+
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const dayName = days[now.getDay()];
+
+    const formattedDate = now.toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' }); // Date in "Month Day, Year" format
+
+    const formattedTime = now.toLocaleTimeString([], { hour: 'numeric', minute: '2-digit', hour12: true }); // 12-hour format without seconds
+
+    const dateTimeString = `${dayName}, ${formattedDate}, ${formattedTime}`;
+
+    document.getElementById('dateAndTime').innerText = dateTimeString;
+};
 
 
-/*$(document).ready(function() {
-  $('. carousel'). carousel({ interval: false,});
-}); 
-*/
+setInterval(updateDateTime, 1000); // Update every second
+updateDateTime(); // Display immediately when the page loads
 
 
-/*$( document ).click(function() {
-  $( "#toggle" ).toggle( "highlight" );
-});
-*/
+
+
+
+
+
+
 
 

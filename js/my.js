9fb736e3c7e7fc88f63fbd98c61f3d7a6a900208
isabeltan <38549280+isabeltan@users.noreply.github.com>
2025-09-08 20:44:42 -0400

    Update my.js

--- js/my.js
+++ js/my.js
@@ -1,3 +1,11 @@
-window.onload = function() {
-    alert("Welcome to our journal!");
-};
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+
+  //draw red marker
+  noStroke();
+  fill(mouseY);
+  circle(mouseX, mouseY, 50);
+}
